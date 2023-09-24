77af5e43df1cb35d238dd649a7b72d6c046c9312
ryanchua00 <ryanchuahj@gmail.com>
2023-09-24 12:34:09 +0800

    300. Longest Increasing Subsequence

---
+++ 1-D Dynamic Programming/lengthOfLIS.py
@@ -0,0 +1,17 @@
+# [10,9,2,5,3,7,101,18]
+# [-1,-1,-1,2,2,4,5,5]
+
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        LIS = [1] * len(nums)
+
+        for i in range(len(nums), -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    nums[i] = max(nums[i], nums[j] + 1)
+
+
+        return max(LIS)

2ddf6a23fa68a6269a7fa275183bd76a515043f3
ryanchua00 <ryanchuahj@gmail.com>
2023-09-29 17:38:41 +0800

    test change

--- 1-D Dynamic Programming/numDecodings.py
+++ 1-D Dynamic Programming/numDecodings.py
@@ -1,19 +1,3 @@
-class Solution:
-    def numDecodings(self, s: str) -> int:
-        # valid digits range from 1 -> 26
-        # 0 is not valid
-        # s is a string of digits
-        # output: the number of ways to map s to characters
-        dp =  { len(s): 1 }
-
-        for i in range(len(s)-1, -1, -1):
-            if s[i] == "0":
-                dp[i] = 0
-            else: 
-                dp[i] = dp[i+1]
-            if (i < len(s) - 1) and (s[i] == "1" or (s[i] == "2" and s[i+1] in "0123456")):
-                dp[i] += dp(i+2)
-        return dp[0]
 
         # dp = { len(s): 1 }
 

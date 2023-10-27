dbb14e070c653c1cbc65c11f27f0b319c6b8cdfa
ryanchua00 <ryanchuahj@gmail.com>
2023-10-27 22:59:31 +0800

    387. First Unique Character in a String

---
+++ Queues/firstUniqChar.py
@@ -0,0 +1,22 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        charMap = {}
+        seen = set()
+        for i in range(len(s)):
+            if s[i] not in charMap:
+                charMap[s[i]] = i 
+            else:
+                seen.add(s[i])
+                charMap.pop(s[i])
+
+        for item in seen:
+            if item in iter(charMap.keys()):
+                print(charMap)
+                charMap.pop(item)
+
+        if len(charMap)> 0:
+            return next(iter(charMap.values()))
+        else:
+            return -1
+        
+print(Solution().firstUniqChar("loveleetcode"))
